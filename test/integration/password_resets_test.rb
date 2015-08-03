eb830715cef7c95a05b8d3939956781e43f96055
Tim Ryan <tryan@valcom.com>
2015-08-03 15:20:55 -0400

    Fix broken password_reset test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -27,6 +27,9 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     # Inactive user
     user.toggle!(:activated)
+    get edit_password_reset_path(user.reset_token, email: user.email)
+    assert_redirected_to root_url
+    user.toggle!(:activated)
     # Right email, wrong token
     get edit_password_reset_path('wrong token', email: user.email)
     assert_redirected_to root_url
