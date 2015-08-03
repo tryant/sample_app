a425f145a5ae9bd2e267bbd3cd46d4139408878d
Tim Ryan <tryan@valcom.com>
2015-08-03 15:07:18 -0400

    Fix typo in password reset controller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -54,7 +54,7 @@ class PasswordResetsController < ApplicationController
 
     def check_expiration
       if @user.password_reset_expired?
-        flash[:dange] = 'Password reset has expired.'
+        flash[:danger] = 'Password reset has expired.'
         redirect_to new_password_reset_url
       end
     end
